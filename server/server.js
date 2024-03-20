da03b951c245a1a4ce8e79f4a61611a7c8290e58
kennadams2 <56715392+kerwin0831@users.noreply.github.com>
2024-03-21 02:52:59 +0800

    Master:

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from "cors";
 import results from "./routes/results.js";
-
+import mongoose from 'mongoose'
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors({
-  origin: 'https://tic-tac-toe-game-kerwin.vercel.app'
+  origin: 'https://tic-tac-toe-game-kerwin.vercel.app',
+  methods: ["POST", "GET"],
+  credentials: true
 }));
 app.use(express.json());
+
+mongoose.connect("mongodb+srv://kerwinfelixreyes:Promax0831@cluster0.s82c9fv.mongodb.net/tic_tac_toe")
 app.use("/results", results);
 
 // start the Express server
