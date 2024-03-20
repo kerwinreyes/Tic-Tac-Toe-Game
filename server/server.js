76ae1781c8b14a6045ede1845dc60c277c3e3374
kennadams2 <56715392+kerwin0831@users.noreply.github.com>
2024-03-21 03:03:05 +0800

    Master:

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import results from "./routes/results.js";
-import mongoose from 'mongoose'
+import http from "http";
+import mongoose from "mongoose";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
@@ -12,10 +13,13 @@ app.use(cors({
 }));
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://kerwinfelixreyes:Promax0831@cluster0.s82c9fv.mongodb.net/tic_tac_toe")
 app.use("/results", results);
+const server = http.createServer(app);
 
-// start the Express server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+mongoose.connect(process.env.MONGO_ATLAS_URL).then(() => {
+  server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}).catch((err) => {
+  process.exit(1);
 });
